1e1f9543c35c41fc941196c2af466d5f1bac32bf
CoreBotics-Lab <hayisyed@gmail.com>
2026-04-09 15:40:50 +0530

    feat(core_description): Add RViz launcher for model visualization
    This commit introduces a launch file and RViz configuration to visualize the `motor_testbench` URDF model.
    
    This provides a complete visualization pipeline, allowing for interactive control and verification of the robot's joint states and TF tree. It automates the process of starting the necessary nodes for viewing and manipulating the model.
    
    Key additions:
    - **`rviz.launch.py`:** A launch file that starts:
        - `robot_state_publisher`: To process the URDF and publish the robot's transform tree (TF2).
        - `joint_state_publisher_gui`: To provide a GUI for interactively controlling the robot's joints.
        - `rviz2`: The primary 3D visualization tool.
    
    - **`rviz_config.rviz`:** A pre-configured RViz session that automatically loads the RobotModel display, sets the correct fixed frame (`base_footprint`), and subscribes to the `/robot_description` topic.

---
+++ src/gazebo_sim/gazebo_core/core_description/launch/rviz.launch.py
@@ -0,0 +1,68 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import Command, PathJoinSubstitution, LaunchConfiguration 
+from launch_ros.parameter_descriptions import ParameterValue
+from ament_index_python.packages import get_package_share_directory
+from launch.conditions import IfCondition
+
+def generate_launch_description():
+
+    # =========================== Arguments ======================
+    use_rviz_arg = DeclareLaunchArgument('rviz', default_value='true',
+                          description='Whether to start RVIZ')
+    rvi_config_arg = DeclareLaunchArgument('rviz_config', default_value='rviz_config.rviz',
+                          description='rviz config file')
+    robot_model_arg = DeclareLaunchArgument('urdf_model', default_value='motor_testbench.urdf.xacro',
+                          description= 'The robot urdf.xacro file to spawn in rviz')
+    use_joint_state_publisher_gui_arg = DeclareLaunchArgument('jst_gui', default_value='true',
+                          description='Whether to start Joint State Publisher GUI')
+
+    # =========================== Paths ==========================
+    urdf_path = PathJoinSubstitution([
+        get_package_share_directory('core_description'),
+        'urdf/motor_testbench',
+        # 'robot_telenex.urdf.xacro'
+        LaunchConfiguration('urdf_model')
+    ])
+    rviz_config_path = PathJoinSubstitution([
+        get_package_share_directory('core_description'),
+        'rviz',
+        LaunchConfiguration('rviz_config')
+    ])
+
+    # ========================= Nodes ============================
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{
+            'robot_description': ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
+        }]
+    )
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen',
+        condition=IfCondition(LaunchConfiguration('jst_gui'))
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_path],
+        condition=IfCondition(LaunchConfiguration('rviz'))
+    )
+
+    # ========================= Launch Description =========================
+    return LaunchDescription([
+        robot_model_arg,
+        use_rviz_arg,        
+        rvi_config_arg,
+        use_joint_state_publisher_gui_arg,        
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        rviz_node
+    ])
