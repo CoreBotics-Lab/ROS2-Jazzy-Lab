7222704836fcfd36ed690a690215edec6d170ce2
CoreBotics-Lab <hayisyed@gmail.com>
2026-05-27 06:27:46 +0000

    feat: implement ros2_control integration with gazebo simulation and add bumperbot_controllers package

---
+++ src/Odometry_and_Control/bumperbot_controllers/launch/controllers.launch.py
@@ -0,0 +1,26 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'joint_state_broadcaster',
+            '--controller-manager', '/controller_manager',
+            ],
+    )
+    
+    velocity_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'velocity_controller',
+            '--controller-manager', '/controller_manager',
+        ],
+    )
+    
+    return LaunchDescription([
+        joint_state_broadcaster_spawner,
+        velocity_controller_spawner,
+    ])
