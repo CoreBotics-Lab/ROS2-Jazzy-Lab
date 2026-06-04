5d0dae02fcf7f60b1041ccb77440ba8521de335a
CoreBotics-Lab <hayisyed@gmail.com>
2026-06-04 10:27:59 +0000

    feat: add diff_drive_kinematics node with controller configuration to launch file

--- src/Odometry_and_Control/bumperbot_controllers/launch/controllers.launch.py
+++ src/Odometry_and_Control/bumperbot_controllers/launch/controllers.launch.py
@@ -1,7 +1,16 @@
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+
+    config_file = os.path.join(
+        get_package_share_directory('bumperbot_controllers'),
+        'config',
+        'bumperbot_controllers.yaml'
+    )
+
     joint_state_broadcaster_spawner = Node(
         package='controller_manager',
         executable='spawner',
@@ -19,8 +28,17 @@ def generate_launch_description():
             '--controller-manager', '/controller_manager',
         ],
     )
+
+    diff_drive_kinematics = Node(
+        package='bumperbot_controllers',
+        executable='diff_drive_kinematics_cpp',
+        name='diff_drive_kinematics',
+        output='screen',
+        parameters=[config_file]
+    )
     
     return LaunchDescription([
         joint_state_broadcaster_spawner,
         velocity_controller_spawner,
+        diff_drive_kinematics
     ])
