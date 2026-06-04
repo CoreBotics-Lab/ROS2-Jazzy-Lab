e460d68e2f4ebdd38efb540cec82b184794b908f
CoreBotics-Lab <hayisyed@gmail.com>
2026-06-04 15:41:44 +0000

    feat: add diff_drive_controller support and toggle between simple and standard controllers via launch arguments

--- src/Odometry_and_Control/bumperbot_controllers/launch/controllers.launch.py
+++ src/Odometry_and_Control/bumperbot_controllers/launch/controllers.launch.py
@@ -1,9 +1,24 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import GroupAction, DeclareLaunchArgument
 from ament_index_python.packages import get_package_share_directory
+from launch.conditions import UnlessCondition, IfCondition
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="True",
+    )
+    use_simple_controller_arg = DeclareLaunchArgument(
+        "use_simple_controller",
+        default_value="True",
+    )
+
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    use_simple_controller = LaunchConfiguration("use_simple_controller")
 
     config_file = os.path.join(
         get_package_share_directory('bumperbot_controllers'),
@@ -19,26 +34,44 @@ def generate_launch_description():
             '--controller-manager', '/controller_manager',
             ],
     )
-    
-    velocity_controller_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=[
-            'velocity_controller',
-            '--controller-manager', '/controller_manager',
+
+    diff_drive_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["bumperbot_controller", 
+                   "--controller-manager", 
+                   "/controller_manager"
         ],
+        condition=UnlessCondition(use_simple_controller)
     )
 
-    diff_drive_kinematics = Node(
-        package='bumperbot_controllers',
-        executable='diff_drive_kinematics_cpp',
-        name='diff_drive_kinematics',
-        output='screen',
-        parameters=[config_file]
+    simple_controller = GroupAction(
+        condition=IfCondition(use_simple_controller),
+        actions=[
+        Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=[
+                'simple_velocity_controller',
+                '--controller-manager', '/controller_manager',
+            ],
+        ),
+
+        Node(
+            package='bumperbot_controllers',
+            executable='diff_drive_kinematics_cpp',
+            name='diff_drive_kinematics',
+            output='screen',
+            parameters=[config_file]
+        )]      
+
     )
     
+    
     return LaunchDescription([
+        use_sim_time_arg,
+        use_simple_controller_arg,
         joint_state_broadcaster_spawner,
-        velocity_controller_spawner,
-        diff_drive_kinematics
+        diff_drive_controller_spawner,
+        simple_controller,
     ])
